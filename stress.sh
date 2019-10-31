ea305efc37ba7bafa36cef3fabb82a819d26de60
aengusrooneygrafana <54800912+aengusrooneygrafana@users.noreply.github.com>
2019-10-31 12:41:14 +0100

    Create stress.sh

---
+++ stress.sh
@@ -0,0 +1,11 @@
+sudo yum -y install epel-release
+sudo yum -y install stress 
+uptime
+stress -c 2 -i 1 -m 1 --vm-bytes 128M -t 10s 
+uptime
+# -c 2 : Spawn two workers spinning on sqrt()
+# -i 1 : Spawn one worker spinning on sync()
+# -m 1 : Spawn one worker spinning on malloc()/free()
+# --vm-bytes 128M : Malloc 128MB per vm worker (default is 256MB)
+# -t 10s : Timeout after ten seconds
+# -v : Be verbose 
